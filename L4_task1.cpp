bb0949d3fa81ca7680b12571fdabf7f601431123
Anastasia Bushina <65818263+shhtol@users.noreply.github.com>
2020-07-25 08:37:54 +0300

    Update L4_task1.cpp

--- L4_task1.cpp
+++ L4_task1.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cassert>
 
 using namespace std;
@@ -27,6 +28,28 @@ public:
     {
         delete[] m_data;
     }
+    void DeleteLast();
+    void insertBefore(int value, int index)
+    {
+        assert(index >= 0 && index <= m_length);
+        int *data = new int[m_length+1];
+        for (int before=0; before < index; ++before)
+            data[before] = m_data[before];
+        data [index] = value;
+        for (int after=index; after < m_length; ++after)
+            data[after+1] = m_data[after];
+        delete[] m_data;
+        m_data = data;
+        ++m_length;
+    }
+    void push_back(int value) { insertBefore(value, m_length); }
+    void SortArray();
+    int& operator[](int index)
+    {
+        assert(index >= 0 && index < m_length);
+        return m_data[index];
+    }
+
 };
 
 void ArrayInt::DeleteLast(){
@@ -39,11 +62,24 @@ void ArrayInt::DeleteLast(){
 } 
 
 
-void SortArray() {
-    int *data = new int[m_length];
+void ArrayInt::SortArray() {
     for(int i = 1; i < m_length; i++)     
-	    for(int j = i; j > 0 && data[j - 1] > data[j]; j--) 
-			int temp = data[j - 1];
-			data[j - 1] = data[j]; 
-			data[j] = temp;
+	    for(int j = i; j > 0 && m_data[j - 1] > m_data[j]; j--) 
+			{
+			int temp = m_data[j - 1];
+			m_data[j - 1] = m_data[j]; 
+			m_data[j] = temp;
+			}
+}
+int main()
+{
+    ArrayInt a;
+    a.push_back(1);
+    a.push_back(3);
+    a.push_back(2);
+    a.SortArray();
+    cout << a[0] << ' ' << a[1] << ' ' << a[2];
+    
+
+    return 0;
 }
